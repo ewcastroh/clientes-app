27a072bd97ee1acb3f6929be2aa547b8e4369373
ewcastroh <ewcastroh10@gmail.com>
2020-01-09 21:29:02 -0500

    Implementing features to add a new item in a new Factura object and creating the new Factura.

--- src/app/facturas/facturas.component.ts
+++ src/app/facturas/facturas.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Factura } from './models/factura';
 import { ClientesService } from '../clientes/clientes.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { startWith, map, flatMap } from 'rxjs/operators';
@@ -9,6 +9,7 @@ import { FacturaService } from './services/factura.service';
 import { Producto } from './models/producto';
 import { MatAutocompleteSelectedEvent } from '@angular/material';
 import { ItemFactura } from './models/item-factura';
+import Swal from 'sweetalert2';
 
 @Component({
 	selector: 'app-facturas',
@@ -25,6 +26,7 @@ export class FacturasComponent implements OnInit {
 
 	constructor(private clientesService: ClientesService,
 				private facturaService: FacturaService,
+				private router: Router,
 				private activatedRoute: ActivatedRoute) { }
 
 	ngOnInit() {
@@ -52,13 +54,64 @@ export class FacturasComponent implements OnInit {
 	seleccionarProducto(event: MatAutocompleteSelectedEvent): void {
 		let producto = event.option.value as Producto;
 		console.log(producto);
-		let nuevoItem = new ItemFactura();
-		nuevoItem.producto = producto;
-		this.factura.items.push(nuevoItem);
+
+		if (this.existeItem(producto.id)) {
+			this.incrementaCantidad(producto.id);
+		} else {
+			let nuevoItem = new ItemFactura();
+			nuevoItem.producto = producto;
+			this.factura.items.push(nuevoItem);
+		}
 
 		this.autoCompleteControl.setValue('');
 		event.option.focus();
 		event.option.deselect();
 	}
 
+	actualizarCantidad(id: number, event: any): void {
+		let cantidad: number = event.target.value as number;
+
+		if (cantidad == 0) {
+			return this.eliminarItemFactura(id);
+		}
+
+		this.factura.items = this.factura.items.map((item: ItemFactura) => {
+			if (id === item.producto.id) {
+				item.cantidad = cantidad;
+			}
+			return item;
+		});
+	}
+
+	existeItem(id: number): boolean {
+		let existe = false;
+		this.factura.items.forEach(item => {
+			if (id === item.producto.id) {
+				existe = true;
+			}
+		});
+		return existe;
+	}
+
+	incrementaCantidad(id: number): void {
+		this.factura.items = this.factura.items.map((item: ItemFactura) => {
+			if (id === item.producto.id) {
+				++item.cantidad;
+			}
+			return item;
+		});
+	}
+
+	eliminarItemFactura(id: number): void {
+		this.factura.items = this.factura.items.filter((item: ItemFactura) => id !== item.producto.id);
+	}
+
+	create(): void {
+		console.log(this.factura);
+
+		this.facturaService.create(this.factura).subscribe(factura => {
+			Swal.fire(this.titulo, `Factura ${factura.descripcion} creada con éxito`, 'success');
+			this.router.navigate(['/clientes']);
+		});
+	}
 }
